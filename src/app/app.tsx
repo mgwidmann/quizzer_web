b26395e446e78747646523b33f38a202405f5267
Matt Widmann <mgwidmann@gmail.com>
2025-06-27 23:19:30 -0400

    Adding chapters 2, 3, 4, and 5 along with a reset data button in order to allow for updates

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,7 +5,7 @@ import {
   ContentLayout,
 } from '@cloudscape-design/components';
 import Quizzer from './quizzer';
-import { loadLocalStorageData, saveLocalStorageData } from './data/localStorage';
+import { eraseLocalStorageData, loadLocalStorageData, saveLocalStorageData } from './data/localStorage';
 
 
 export default function Quiz() {
@@ -23,12 +23,23 @@ export default function Quiz() {
     document.body.removeChild(a); // Clean up
   }
 
+  const reset = () => {
+    const erase = confirm("Are you sure you want to erase any changes you have made?!? This action is not reversible!");
+    if (erase) {
+      // ensure an exported copy
+      exportData();
+      eraseLocalStorageData();
+      alert("Data has been reset, you must refresh the page to see the changes.");
+    }
+  }
+
   return (
     <AppLayout
       navigationHide={true}
       toolsHide={true}
       content={
         <ContentLayout>
+          <Button onClick={reset}>Reset Data</Button>
           <Button onClick={exportData}>Export</Button>
           <Quizzer quiz={data} editMode={editMode} setEditMode={setEditMode} setQuiz={(quiz) => { saveLocalStorageData(quiz); setData(quiz) }} />
         </ContentLayout>
--- src/app/data/localStorage.ts
+++ src/app/data/localStorage.ts
@@ -1,4 +1,8 @@
 import chapter1 from '../../chapter1.json';
+import chapter2 from '../../chapter2.json';
+import chapter3 from '../../chapter3.json';
+import chapter4 from '../../chapter4.json';
+import chapter5 from '../../chapter5.json';
 import type { Quiz } from '../file/quiz';
 
 
@@ -6,15 +10,20 @@ export function loadLocalStorageData() {
   return () => {
     const savedData = localStorage.getItem('quizzerData');
     return savedData ? JSON.parse(savedData) : { chapters: [
-      chapter1, 
-      { name: 'Розділ 2. Історія і теорія психології та соціології', sections: []}, 
-      { name: 'Розділ 3', sections: []}, 
-      { name: 'Розділ 4. Прикладні аспекти психології та соціології', sections: []}, 
-      { name: 'Розділ 5. Психологічні та соціологічні виміри суспільства.', sections: []}] 
+        chapter1, 
+        chapter2,
+        chapter3,
+        chapter4,
+        chapter5, 
+      ] 
     };
   }
 }
 
 export function saveLocalStorageData(quiz: Quiz) {
   localStorage.setItem('quizzerData', JSON.stringify(quiz));
+}
+
+export function eraseLocalStorageData() {
+  localStorage.removeItem('quizzerData');
 }
